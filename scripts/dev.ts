ea0b2818263bbb3df767f22c9507d38b3abde9d0
Erick Christian <erickchristian48@gmail.com>
2025-03-05 15:53:03 +0700

    chore: Use tsx for dev script and update dependencies

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -1,19 +1,16 @@
 import concurrently from "concurrently"
 
-const result = concurrently(
-  [
-    {
-      command: "vite",
-      name: "vite",
-      prefixColor: "yellow",
-    },
-    {
-      command: "./pocketbase serve --dev",
-      name: "pb",
-      prefixColor: "white",
-    },
-  ],
-  {},
-)
+const result = concurrently([
+  {
+    command: "vite",
+    name: "vite",
+    prefixColor: "yellow",
+  },
+  {
+    command: "./pocketbase serve --dev",
+    name: "pb",
+    prefixColor: "white",
+  },
+])
 
 await result.result
