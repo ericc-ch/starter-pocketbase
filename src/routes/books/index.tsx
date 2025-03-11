12ffaf5c6c31f5925231fd76a65d9c3c3a59b2e6
Erick Christian <erickchristian48@gmail.com>
2025-03-11 09:09:39 +0700

    feat: Update fly.toml and books route index file

--- src/routes/books/index.tsx
+++ src/routes/books/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router"
 
-export const Route = createFileRoute("/books/")({
+export const Route = createFileRoute("/books")({
   component: Books,
 })
 
